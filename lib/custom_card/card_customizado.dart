3b94480b3ec31c490e463cfc80a81e8c93e0afab
LeBaleiro <lehbaleiro@gmail.com>
2024-07-24 16:13:45 -0300

    wip

--- lib/custom_card/card_customizado.dart
+++ lib/custom_card/card_customizado.dart
@@ -2,37 +2,6 @@ import 'package:adaptive/custom_card/layouts/alto_layout.dart';
 import 'package:adaptive/custom_card/layouts/largo_layout.dart';
 import 'package:flutter/material.dart';
 
-// class CardCustomizado extends StatelessWidget {
-//   ...
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Card(
-//       shape: RoundedRectangleBorder(
-//         borderRadius: BorderRadius.circular(16.0),
-//       ),
-//       child: LayoutBuilder(builder: (context, constraints) {
-//         final isMoreTallThanWide = constraints.maxHeight > constraints.maxWidth;
-//         if (isMoreTallThanWide) {
-//           return AltoLayout(
-//             imageUrl: urlImagem,
-//             caption: titulo,
-//             description: descricao,
-//             tagLabel: textoTag,
-//           );
-//         } else {
-//           return LargoLayout(
-//             imageUrl: urlImagem,
-//             caption: titulo,
-//             description: descricao,
-//             tagLabel: textoTag,
-//           );
-//         }
-//       }),
-//     );
-//   }
-// }
-
 class CardCustomizado extends StatelessWidget {
   const CardCustomizado({
     super.key,
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,31 +1,12 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:ui' as ui;
+
 import 'package:adaptive/custom_dialog/custom_dialog.dart';
 import 'package:adaptive/image_list/grid_ipsum.dart';
 import 'package:adaptive/navigation/bottom_nav_bar.dart';
 import 'package:adaptive/navigation/side_nav_rail.dart';
 import 'package:flutter/material.dart';
-
-// class HomePage extends StatefulWidget {
-//   ...
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final useSideNavRail = MediaQuery.sizeOf(context).width >= 600;
-//     return Scaffold(
-//       appBar: AppBar(...),
-//       bottomNavigationBar: useSideNavRail
-//           ? null
-//           : BottomNavBar(...),
-//       floatingActionButton: FloatingActionButton(...),
-//       body: Row(
-//         children: [
-//           if (useSideNavRail) SideNavRail(...),
-//           const Expanded(child: GridIpsum()),
-//         ],
-//       ),
-//     );
-//   }
-// }
+import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -41,8 +22,46 @@ class _HomePageState extends State<HomePage> {
     setState(() => indexSelected = index);
   }
 
+  /// Não faça isso:
+  // void handleSizeChange(Size size) {
+  //   final orientations = <DeviceOrientation>[];
+  //   orientations.addAll([
+  //     DeviceOrientation.portraitUp,
+  //     DeviceOrientation.portraitDown,
+  //   ]);
+  //   bool isCompact = size.shortestSide < 600 && size != Size.zero;
+  //   if (!isCompact) {
+  //     orientations.addAll([
+  //       DeviceOrientation.landscapeLeft,
+  //       DeviceOrientation.landscapeRight,
+  //     ]);
+  //   }
+  //   SystemChrome.setPreferredOrientations(orientations);
+  // }
+
+  /// Prefira deixar liberada todas as orientações, mas caso precise travar, faça isso:
+  ui.Display get display => ui.PlatformDispatcher.instance.displays.first;
+
+  void handleSizeChange(Size size) {
+    final orientations = <DeviceOrientation>[];
+    orientations.addAll([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    bool isCompact = display.size.shortestSide / display.devicePixelRatio < 600;
+    if (!isCompact) {
+      orientations.addAll([
+        DeviceOrientation.landscapeLeft,
+        DeviceOrientation.landscapeRight,
+      ]);
+    }
+    SystemChrome.setPreferredOrientations(orientations);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
+    handleSizeChange(size);
     final useSideNavRail = MediaQuery.sizeOf(context).width >= 600;
     return Scaffold(
       appBar: AppBar(
